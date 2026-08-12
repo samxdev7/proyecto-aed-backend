50de1309197a233069a230ab44d69df0cf15b1ee
Aneural <m.aleman9922@gmail.com>
2026-08-11 22:14:25 -0600

    Initial commit: Spring Boot template (CNM API)

---
+++ src/main/java/com/cnm/backend/CnmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cnm.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cnm/backend/CnmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnm.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
